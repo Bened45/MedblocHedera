c9f11e1425f7a37ce3af903dac9805f4a4b91e86
Bened45 <benedoffice@gmail.com>
2025-10-31 21:53:17 +0100

    z

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -65,7 +65,7 @@ function LoginPage() {
             </div>
             <div className="mb-6">
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="privateKey">
-                Clé Privée (simulée)
+                Clé Privée
               </label>
               <input 
                 className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline ${errors.privateKey ? 'border-red-500' : ''}`}
